76b214010606e54177f6cbb1d261660a56694c7d
Viktoria Petrenko <vikuacz@seznam.cz>
2019-10-19 18:20:01 +0200

    note-app: added info on the page  About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,12 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 
-export const About = ()=>{
-    return (
-        <Fragment>
-            <h1>About page</h1>
-        </Fragment>
-    )
-};
+export const About = ()=>(
+    <div className="jumbotron ">
+                <div className="container">
+                    <h1 className="display-4">Best Note Taking App</h1>
+                    <p className="lead">Note taking apps are the digital equivalent of notebooks, and because they're digital, they can <strong>do more for you </strong>than paper ever could.</p>
+                </div>
+    </div>
+
+
+);
