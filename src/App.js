dea2383b942470301902f0a60688c88ad435844e
Viktoria Petrenko <vikuacz@seznam.cz>
2019-10-19 17:51:47 +0200

    note-app: added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Navbar} from "./components/Navbar";
 
 function App() {
   return (
       <BrowserRouter>
+          <Navbar/>
           <div className="container pt-4">
               <Switch>
-                  <Route/>
+                  <Route path={'/'} exact component={Home}/>
+                  <Route path={'/about'} exact component={About}/>
               </Switch>
           </div>
       </BrowserRouter>
